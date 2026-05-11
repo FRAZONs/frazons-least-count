054c18d63602fe94ef67b9242d5da43535c197ef
FRAZONs <mdarhaankhan220@gmail.com>
2026-05-11 17:41:57 +0530

    Updated PWA icon names

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,64 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+import react from "@vitejs/plugin-react";
+
+import { VitePWA }
+  from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react()],
-})
+
+  plugins: [
+
+    react(),
+
+    VitePWA({
+
+      registerType:
+        "autoUpdate",
+
+      manifest: {
+
+        name:
+          "Frazon's Least Count",
+
+        short_name:
+          "Least Count",
+
+        description:
+          "Neon card game tracker",
+
+        theme_color:
+          "#0d0d0d",
+
+        background_color:
+          "#0d0d0d",
+
+        display:
+          "standalone",
+
+        start_url:
+          "/",
+
+        icons: [
+
+          {
+            src: "new icon-512.png",
+
+            sizes: "192x192",
+
+            type: "image/png"
+          },
+
+          {
+            src: "new icon-512.png",
+
+            sizes: "512x512",
+
+            type: "image/png"
+          }
+
+        ]
+      }
+    })
+  ]
+});
